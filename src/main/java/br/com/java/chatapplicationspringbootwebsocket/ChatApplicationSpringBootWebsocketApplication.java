431ee24b55c15774acce299fa9dca3c487e01ef4
Diego Pereira <diegoteixeirapereira@hotmail.com>
2021-07-06 22:45:26 -0300

    first commit

---
+++ src/main/java/br/com/java/chatapplicationspringbootwebsocket/ChatApplicationSpringBootWebsocketApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.chatapplicationspringbootwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationSpringBootWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplicationSpringBootWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/chatapplicationspringbootwebsocket/ChatApplicationSpringBootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.chatapplicationspringbootwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationSpringBootWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
